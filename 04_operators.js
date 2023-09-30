2015bd8842c095990ac62a2a3f6f35ebab9a5e5c
Vedantnawale <vedantnawale6@gmail.com>
2023-09-30 22:53:19 +0530

    Operators

---
+++ 04_operators.js
@@ -0,0 +1,44 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// Operators
+/*
+console.log(2+2);
+console.log(2*2);
+console.log(2-2);
+console.log(2**2);
+console.log(2%2);
+console.log(2/2);
+*/
+
+let str1 = "hello"
+let str2 = " Vedant"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// MDN Example
+let x = 3;
+let y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+let b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
