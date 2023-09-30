5686d3d5c07d9a5937da77b83565f2ca1a8bc61f
Vedantnawale <vedantnawale6@gmail.com>
2023-09-30 16:42:03 +0530

    conversion datatypes

---
+++ 03_conversion.js
@@ -0,0 +1,19 @@
+// Datatype Conversion
+
+// let score = "33abc" // NaN
+// let score = null // 0
+// let score = undefined // NaN
+let score = true
+
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber))
+console.log(valueInNumber) // NaN --> Not a Number
+
+let isLoggedin = "om"
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn)
