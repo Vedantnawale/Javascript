c28de03819070256548b010d78d29e72791b1f1b
Vedantnawale <vedantnawale6@gmail.com>
2023-10-01 22:43:39 +0530

    Number and Maths

---
+++ 08_Number_and_Maths.js
@@ -0,0 +1,41 @@
+// numbers 
+
+let score = 400
+
+console.log(score)
+
+let balance = new Number(100.)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+let otherNumber = 123.63343
+
+// console.log(otherNumber.toPrecision(3));
+
+let hundreads = 1000000
+// console.log(hundreads.toLocaleString('en-IN'));
+
+// ++++++++++++ Maths ++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-29));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 2, 5, 6, 8));
+// console.log(Math.max(8,4, 5, 4, 2));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+let min = 10
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
