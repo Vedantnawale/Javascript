11c3d86b50ba72b73d98b7450a7c89dacb29b7af
Vedantnawale <vedantnawale6@gmail.com>
2023-10-13 23:14:25 +0530

    ControlFlow

---
+++ 22_ControFlow.js
@@ -0,0 +1,29 @@
+// if
+
+// let isuserLogedin = true
+// let temparute = 41
+
+// if (temparute < 50) {
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("tempearure is grater than 50");
+// }
+// console.log("executed");
+// <, >, >= , <=, ==, !=, ===
+
+let balance = 50
+
+if(balance==50) console.log(balance); // impicit scope
+
+// switch case
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
