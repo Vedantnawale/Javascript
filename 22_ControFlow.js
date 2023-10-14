21e323e7e1c79b3eaae1b14f1fd3094e97a9424c
Vedantnawale <vedantnawale6@gmail.com>
2023-10-14 09:43:52 +0530

    add some new control flow

--- 22_ControFlow.js
+++ 22_ControFlow.js
@@ -18,12 +18,35 @@ if(balance==50) console.log(balance); // impicit scope
 
 // switch case
 
-switch (key) {
-    case value:
+// switch (key) {
+//     case value:
         
-        break;
+//         break;
 
-    default:
-        break;
-}
+//     default:
+//         break;
+// }
 
+// access object in condition
+
+// let myObj = {}
+
+// if(Object.keys(myObj).length === 0){
+//     console.log("Object is empty");
+// }
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+val1 = undefined ?? 10
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+// Terinary Operator
+
+// condition ? true : false
