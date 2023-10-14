6902c95c73209e4fbfcd44c74cb9b91294eb6adf
Vedantnawale <vedantnawale6@gmail.com>
2023-10-14 23:11:29 +0530

    loop

---
+++ 23_loop.js
@@ -0,0 +1,32 @@
+// for
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     if(element == 5){
+//         console.log("5 is best number");
+//     }
+//     console.log(element); 
+// }
+
+// for (let i = 1; i <= 10; i++) {
+// //     console.log(`Outer Loop value i : ${i}`);
+// //    for (let j = 1; j <= 10; j++) {
+// //     // console.log(`Inner loop value j : ${j} and Inner loop value i : ${i}`);
+// //     console.log(i + "*" + j + "=" + i*j);
+//    }
+    
+// }
+
+//  break --> break aage nahi badhega
+// continue --> wo chhodke baki sab run hoga
+
+// While and do while
+let i = 0
+while (i <= 10) {
+    console.log(`Value of i is ${i}`);
+    i = i + 2
+}
+
+// do {
+    
+// } while (condition); ek bar to jarur run hoga
