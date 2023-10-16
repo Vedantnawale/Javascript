51d6d28023bc152af6eae38cb199f43d7cfe5246
Vedantnawale <vedantnawale6@gmail.com>
2023-10-16 23:29:38 +0530

    basics dom

---
+++ dom_manipulation/26_basicDom.js
@@ -0,0 +1,16 @@
+/*
+document.getElementById('title')
+document.getElementsByClassName('heading')
+document.querySelector('h1')
+document.querySelector('#title')// id
+document.querySelector('.heading') // class
+document.querySelectorAll('h1')
+document.querySelectorAll('.heading')
+
+title.textContent 
+'DOM Learning on Chai Aur Code'
+title.innerHTML
+'DOM Learning on Chai Aur Code'
+title.innerText --> display none hone ke baad nahi dikhta
+'DOM Learning on Chai Aur Code'
+*/
