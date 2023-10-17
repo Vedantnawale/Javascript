973d463ff1147c7798a888c5065ac22cbce692bd
Vedantnawale <vedantnawale6@gmail.com>
2023-10-17 21:48:39 +0530

    DigitalClock

---
+++ basic_projects_js/03_DigitalClock.js
@@ -0,0 +1,10 @@
+let clock = document.getElementById('clock')
+// document.querySelector('#clock')
+
+
+
+setInterval(function () {
+    let date = new Date()
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString()
+}, 1000)
