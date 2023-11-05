6a242b6b93d579261eb4d59ab92351f25efe1b61
Vedantnawale <vedantnawale6@gmail.com>
2023-11-04 21:02:40 -0400

    async

---
+++ 04_events/31_async.js
@@ -0,0 +1,6 @@
+/* blocking code vs non blocking code
+        |                   |
+block the flow          Does not block execeution
+ of program                     
+      |                             |
+Read file Sync                 Read file async */
