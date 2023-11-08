9b07d80de9c454060d1c3919fda80a975e749e58
Vedantnawale <vedantnawale6@gmail.com>
2023-11-07 21:47:54 -0500

    Introduction to OOP

---
+++ 05_classes_ &_oop/38_t_intro_oop.js
@@ -0,0 +1,28 @@
+ // Javascript and Classes
+
+ // OOP
+
+ /*
+  Object
+  - Collection of properties and methods
+  - toLowerCase
+
+  Methods
+  - Methods are actions that can be performed on objects.
+
+  ## why use OOP
+
+  ## parts of OOP
+  - Object Literal
+
+  - Constructor Function
+  - Proottypes
+  - Classes
+  - Instances (new, this)
+
+  ## 4 Pillars
+  - Abstraction
+  - Encapsulation
+  - Inheritance
+  - Polymorphism
+ */
