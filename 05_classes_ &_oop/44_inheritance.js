237e64958ee05f098ee246bc857d913b27501fb8
Vedantnawale <vedantnawale6@gmail.com>
2023-11-08 12:41:51 -0500

    Inheritance

---
+++ 05_classes_ &_oop/44_inheritance.js
@@ -0,0 +1,30 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+// Teacher Childclass where User is parentclass
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+    addCourse(){
+        console.log(`new course was added by ${this.username}`);
+    }
+}
+
+let chai = new Teacher("chai", "chai@gmail.com","123")
+
+chai.logMe()
+
+let masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai === Teacher);
+console.log(chai instanceof Teacher);
